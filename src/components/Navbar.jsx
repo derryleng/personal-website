e34e921882e07a0bae231ce8dfef1a4ec8c9d4de
Derry Leng <derry.leng@gmail.com>
2023-07-15 15:23:46 +0100

    Minimal navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,150 +1,81 @@
-// import NavbarLeft from "./NavbarLeft";
-// import NavbarRight from "./NavbarRight";
-import { Disclosure, Menu, Transition } from '@headlessui/react';
-import { Bars3Icon, BellIcon, MoonIcon, SunIcon, XMarkIcon } from '@heroicons/react/24/outline';
-import { Fragment } from 'react';
-import navlinks from "../data/navlinks";
-import myFace from "/assets/me_2021_365_365.jpg";
+import { useEffect, useState } from "react";
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
 
 function Navbar() {
+    const [theme, setTheme] = useState(null);
+
+	useEffect(() => {
+		if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+			setTheme('dark');
+		} else {
+			setTheme('light');
+		}
+	}, []);
+
+	const handleThemeSwitch = () => {
+		setTheme(theme === 'dark' ? 'light' : 'dark');
+	};
+
+	useEffect(() => {
+		if (theme === 'dark') {
+			document.documentElement.classList.add('dark');
+		} else {
+			document.documentElement.classList.remove('dark');
+		}
+	}, [theme]);
+
+	const sun = (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			strokeWidth={1.5}
+			stroke="black"
+		>
+			<path
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+			/>
+		</svg>
+	);
+
+	const moon = (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			strokeWidth={1.5}
+			stroke="white"
+		>
+			<path
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+			/>
+		</svg>
+	);
+
     return (
-        <div className="sticky top-0 z-50">
-            <Disclosure as="nav" className="bg-gray-800">
-                {({ open }) => (
-                    <>
-                        <div className="px-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
-                            <div className="relative flex items-center justify-between h-11">
-                                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                                    {/* Mobile menu button*/}
-                                    <Disclosure.Button className="inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                                        <span className="sr-only">Open main menu</span>
-                                        {open ? (
-                                            <XMarkIcon className="block w-6 h-6" aria-hidden="true" />
-                                        ) : (
-                                            <Bars3Icon className="block w-6 h-6" aria-hidden="true" />
-                                        )}
-                                    </Disclosure.Button>
-                                </div>
-                                <div className="flex items-center justify-center flex-1 sm:items-stretch sm:justify-start">
-                                    <div className="flex items-center flex-shrink-0">
-                                        <img
-                                            className="w-8 h-8 rounded-full"
-                                            src={myFace}
-                                            alt="Derry"
-                                        />
-                                    </div>
-                                    <div className="hidden sm:ml-6 sm:block">
-                                        <div className="flex space-x-4">
-                                            {navlinks.map((item) => (
-                                                <a
-                                                    key={item.name}
-                                                    href={item.href}
-                                                    className={classNames(
-                                                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                                        'rounded-md px-3 py-2 text-sm font-medium'
-                                                    )}
-                                                    aria-current={item.current ? 'page' : undefined}
-                                                >
-                                                    {item.name}
-                                                </a>
-                                            ))}
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                                    <button
-                                        type="button"
-                                        className="p-1 text-gray-400 bg-gray-800 rounded-full hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                                    >
-                                        <span className="sr-only">View notifications</span>
-                                        <BellIcon className="w-6 h-6" aria-hidden="true" />
-                                    </button>
+        <div className="sticky top-0 z-50 pt-2 text-black bg-white dark:bg-slate-800 dark:text-stone-200">
+            <div className="flex flex-row mx-5">
+                <div className="flex items-center space-x-3">
+                    
+                </div>
+                <div className="flex items-center ml-auto">
+                    <button
+                        type="button"
+                        onClick={handleThemeSwitch}
+                        className="w-6 h-6 rounded-md bg-violet-500 dark:bg-orange-500"
+                    >
+                        {theme === "dark" ? sun : moon}
+                    </button>
+                </div>
 
-                                    {/* Profile dropdown */}
-                                    {/* <Menu as="div" className="relative ml-3">
-                                        <div>
-                                            <Menu.Button className="flex text-sm bg-gray-800 rounded-full focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                                                <span className="sr-only">Open user menu</span>
-                                                <img
-                                                    className="w-8 h-8 rounded-full"
-                                                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                                    alt=""
-                                                />
-                                            </Menu.Button>
-                                        </div>
-                                        <Transition
-                                            as={Fragment}
-                                            enter="transition ease-out duration-100"
-                                            enterFrom="transform opacity-0 scale-95"
-                                            enterTo="transform opacity-100 scale-100"
-                                            leave="transition ease-in duration-75"
-                                            leaveFrom="transform opacity-100 scale-100"
-                                            leaveTo="transform opacity-0 scale-95"
-                                        >
-                                            <Menu.Items className="absolute right-0 z-10 w-48 py-1 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                                <Menu.Item>
-                                                    {({ active }) => (
-                                                        <a
-                                                            href="#"
-                                                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                                                        >
-                                                            Your Profile
-                                                        </a>
-                                                    )}
-                                                </Menu.Item>
-                                                <Menu.Item>
-                                                    {({ active }) => (
-                                                        <a
-                                                            href="#"
-                                                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                                                        >
-                                                            Settings
-                                                        </a>
-                                                    )}
-                                                </Menu.Item>
-                                                <Menu.Item>
-                                                    {({ active }) => (
-                                                        <a
-                                                            href="#"
-                                                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                                                        >
-                                                            Sign out
-                                                        </a>
-                                                    )}
-                                                </Menu.Item>
-                                            </Menu.Items>
-                                        </Transition>
-                                    </Menu> */}
-                                </div>
-                            </div>
-                        </div>
-
-                        <Disclosure.Panel className="sm:hidden">
-                            <div className="px-2 pt-2 pb-3 space-y-1">
-                                {navlinks.map((item) => (
-                                    <Disclosure.Button
-                                        key={item.name}
-                                        as="a"
-                                        href={item.href}
-                                        className={classNames(
-                                            item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                            'block rounded-md px-3 py-2 text-base font-medium'
-                                        )}
-                                        aria-current={item.current ? 'page' : undefined}
-                                    >
-                                        {item.name}
-                                    </Disclosure.Button>
-                                ))}
-                            </div>
-                        </Disclosure.Panel>
-                    </>
-                )}
-            </Disclosure>
-            <div className="h-2 bg-gradient-to-b from-gray-800 to-transparent" />
+            </div>
         </div>
     )
 }
