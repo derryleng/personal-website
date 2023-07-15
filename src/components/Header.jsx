8d27deea356de34ae86725f6bd7bbd242b8fafbc
Derry Leng <derry.leng@gmail.com>
2023-07-15 18:53:00 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ function Header() {
             />
             <h1 className="mb-2 text-4xl font-bold md:text-7xl md:mb-4">Derry Leng</h1>
             <p className="mb-3 font-lg text-md md:text-xl">Data Analyst and Web Developer</p>
-            <p className="max-w-xl mb-6 text-sm text-justify">I am a data analyst with experience applying R, Python, SQL on data-driven projects within consulting firms. I am also a full stack developer with experience building web applications using technologies such as React, Django, R Shiny and more, as well as hosting and maintaining these applications on various cloud providers. I have worked solo and in small agile teams using CI/CD. I am fascinated by learning new things in the world of Finance and Tech.</p>
+            <p className="max-w-xl mb-6 text-sm text-justify">I am a data analyst with experience applying R, Python, SQL on data-driven projects within consulting firms, as well as a full stack developer with experience building web applications using technologies such as React, Django, R Shiny and more - hosting and maintaining these applications on various cloud providers. I have worked solo and in small agile teams using CI/CD. I am fascinated by learning new things in the world of Finance and Tech.</p>
         </div>
     )
 }
