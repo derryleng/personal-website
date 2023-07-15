9341ffcf25203ac8cc9f3444fc61c69e4c91cef5
Derry Leng <derry.leng@gmail.com>
2023-07-15 23:37:31 +0100

    Timeline okay for now

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -4,7 +4,7 @@ import TimelineItem from "./TimelineItem";
 
 function Timeline() {
     return (
-        <div className="flex flex-col justify-center my-20 md:flex-row">
+        <div className="flex flex-col justify-center px-2 my-20 md:flex-row md:px-0">
             <div className="w-full md:w-7/12">
             <SectionTitle>Timeline</SectionTitle>
                 {timeline.map(item => (
@@ -12,7 +12,11 @@ function Timeline() {
                         dateStart={item.dateStart}
                         dateEnd={item.dateEnd}
                         title={item.title}
+                        organisation={item.organisation}
+                        role={item.role}
+                        location={item.location}
                         description={item.description}
+                        additionalList={item.additionalList}
                     />
                 ))}
             </div>
--- src/components/TimelineItem.jsx
+++ src/components/TimelineItem.jsx
@@ -1,23 +1,41 @@
-function TimelineItem({ dateStart, dateEnd, title, description }) {
+function TimelineItem({ dateStart, dateEnd, title, organisation, role, location, description, additionalList }) {
     return (
         <div>
             <ol className="relative flex flex-col border-l md-flex-row border-stone-200">
-                <li className="mb-10 ml-4">
+                <li className="ml-4 mb-14">
                     <div className="absolute w-3 h-3 bg-stone-200 rounded-full mt-1.5 -left-1.5 border border-white" />
-                    <p className="flex flex-row flex-wrap items-center justify-start gap-4 text-xs md:text-sm">
-                        <span className="inline-block px-2 py-1 font-semibold text-white rounded-md bg-stone-900">
+                    <p className="flex flex-row flex-wrap items-center justify-start gap-4 mb-1 text-xs md:text-sm">
+                        <span className="inline-block px-2 py-1 font-semibold text-white rounded-md shadow-2xl bg-stone-900">
                             {dateStart} - {dateEnd}
                         </span>
-                        <h3 className="text-lg font-semibold text-stone-900 dark:text-white">
+                        <p className="text-xl font-semibold transition-colors duration-500 text-stone-900 dark:text-white">
                             {title}
-                        </h3>
-                        <div className="my-1 text-sm font-normal leading-none text-stone-400 dark:white">
+                        </p>
+                        <div className="mt-1 text-sm font-normal leading-none text-stone-400 dark:white">
                             {dateEnd - dateStart} years
                         </div>
                     </p>
-                    <p className="my-2 text-base font-normal text-stone-500 dark:text-white">
-                        {description}
-                    </p>
+                    <div className="p-6 rounded-md md:shadow-2xl">
+                        <p className="text-lg font-semibold transition-colors duration-500 text-stone-900 dark:text-white">
+                            {organisation}
+                        </p>
+                        <p className="text-lg font-normal">
+                            {role}
+                        </p>
+                        <p className="mb-2 font-light text-md">
+                            {location}
+                        </p>
+                        <p className="text-base font-normal text-justify transition-colors duration-500 text-stone-700 dark:text-slate-300">
+                            {description}
+                        </p>
+                        {additionalList && additionalList.length > 0 && (
+                            <ul className="pl-6 mt-2 text-base font-normal list-disc text-stone-700 dark:text-slate-300">
+                                {additionalList.map((item, index) => (
+                                    <li key={index}>{item}</li>
+                                ))}
+                            </ul>
+                        )}
+                    </div>
                 </li>
             </ol>
         </div>
