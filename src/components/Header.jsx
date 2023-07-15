c418ec23ef9831f3a0dd9f34cdf99f93997c321b
Derry Leng <derry.leng@gmail.com>
2023-07-15 18:03:31 +0100

    Edit header and footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,13 +5,13 @@ function Header() {
     return (
         <div className="flex flex-col items-center justify-center pt-20 pb-6 text-center">
             <img
-                className="w-20 h-20 rounded-full ring-2 ring-stone-500 border-stone-500 border-3"
+                className="w-20 h-20 rounded-full md:w-40 md:h-40 ring-2 ring-slate-200 border-stone-500 border-3"
                 src={myFace}
                 alt="Derry"
             />
-            <h1 className="mb-2 text-4xl font-bold md:text-7xl md:mb-6">Derry Leng</h1>
-            <p className="mb-3 font-lg text-md md:text-xl">Financial Data Analytics</p>
-            <p className="max-w-xl mb-6 text-sm text-justify">Data analyst and full stack web developer with experience applying analytical tools such as R, Python, SQL, and web development tools within an agile environment. Recognised for inspiring growth within the team and new technology adoption within the firm. I am devoted to lifelong learning and determined to generate innovative solutions to tackle business challenges.</p>
+            <h1 className="mb-2 text-4xl font-bold md:text-7xl md:mb-4">Derry Leng</h1>
+            <p className="mb-3 font-lg text-md md:text-xl">Data Analyst and Web Developer</p>
+            <p className="max-w-xl mb-6 text-sm text-justify">I am a data analyst with experience applying R, Python, SQL on data-driven projects within consulting firms. I am also a full stack developer with experience building web applications using technologies such as React, Django, R Shiny and more, as well as hosting and maintaining these applications on various cloud providers. I have worked solo and in small agile teams using CI/CD. I am fascinated by learning new things in the world of Finance and Tech.</p>
         </div>
     )
 }
