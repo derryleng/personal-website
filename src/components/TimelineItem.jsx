7fc110343e85d23e8d3cc3e0c907be7c4802747d
Derry Leng <derry.leng@gmail.com>
2023-07-15 15:50:15 +0100

    Dark mode fixes

--- src/components/TimelineItem.jsx
+++ src/components/TimelineItem.jsx
@@ -8,14 +8,14 @@ function TimelineItem({ dateStart, dateEnd, title, description }) {
                         <span className="inline-block px-2 py-1 font-semibold text-white rounded-md bg-stone-900">
                             {dateStart} - {dateEnd}
                         </span>
-                        <h3 className="text-lg font-semibold text-stone-900">
+                        <h3 className="text-lg font-semibold text-stone-900 dark:text-white">
                             {title}
                         </h3>
-                        <div className="my-1 text-sm font-normal leading-none text-stone-400">
+                        <div className="my-1 text-sm font-normal leading-none text-stone-400 dark:white">
                             {dateEnd - dateStart} years
                         </div>
                     </p>
-                    <p className="my-2 text-base font-normal text-stone-500">
+                    <p className="my-2 text-base font-normal text-stone-500 dark:text-white">
                         {description}
                     </p>
                 </li>
